d18d6795248ee90da8fb25e6913d9c325eb00414
Markus Machel <machelmarkus@gmail.com>
2025-12-13 13:50:02 -0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://voluble-dusk-6cc153.netlify.app/",
+  integrations: [preact()],
 });
